8682c0625e90c6bdba21d67238d4efb55661dc71
gopalksahoo063 <gopal@havahavai.com>
2024-11-15 00:21:37 +0530

    removed phishing of api keys

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,13 +5,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js')
 
 // Initialize the Firebase app in the service worker by passing in the messaging sender ID
 firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyD3ueXI94zOMu6DQpP-4eNH6lK7ctM61Q8",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "flixyai.firebaseapp.com",
     projectId: "flixyai",
     storageBucket: "flixyai.firebasestorage.app",
     messagingSenderId: "194441512510",
-    appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:194441512510:web:c3a5e777e097547228736f",
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "G-7YBWR61GP7",
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background messages.
